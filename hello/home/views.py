9c1f2df3e67384a989cba64b608bd6cde7d9a33b
serz4862 <sauravkumar4862@gmail.com>
2023-04-20 01:28:06 +0530

    firs commit

---
+++ hello/home/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("this is homepage")
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def services(request):
+    return HttpResponse("this is Services page")
+
+def contact(request):
+    return HttpResponse("this is Contact page")
